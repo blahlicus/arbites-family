ad6f4d26dce5b3f8e4d6587a802b71b068f77668
blahlicus <blahlicus@gmail.com>
2016-01-01 06:35:48 +0800

    added: class clkey

---
+++ Arbites4/Arbites4/Arbites4/ClKey.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbites4
+{
+    public class ClKey
+    {
+        public char val { get; set; }
+        public Byte ktype { get; set; }
+        public string display { get; set; }
+
+        public ClKey()
+        {
+            val = (char)0;
+            display = "Null";
+            ktype = 255;
+        }
+
+        public ClKey(char val, Byte ktype, string display)
+        {
+            this.val = val;
+            this.ktype = ktype;
+            this.display = display;
+        }
+    }
+}
