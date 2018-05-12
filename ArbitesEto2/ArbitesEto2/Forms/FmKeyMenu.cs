26a0ff203d67ae045286137df7e701ff5dcbdb46
CarlOlson <CarlOlson@users.noreply.github.com>
2018-05-12 16:50:03 -0500

    replace tabs with dropdown, add search

--- ArbitesEto2/ArbitesEto2/Forms/FmKeyMenu.cs
+++ ArbitesEto2/ArbitesEto2/Forms/FmKeyMenu.cs
@@ -7,58 +7,99 @@ using System.Linq;
 
 namespace ArbitesEto2
 {
-    public sealed partial class FmKeyMenu
+    public sealed class FmKeyMenu : Form
     {
+        private DropDown DropDown;
+        private SearchBox SearchBox;
+        private ListBox ListBox;
+        private int GroupIndex = 0;
+
         public FmKeyMenu()
         {
-            InitializeComponent();
+            Title = "Key Menu - Select a key from the menu below";
+            Height = 640;
+            Width = 860;
+            Topmost = MdConfig.Main.KeyMenuTopmost;
+            // Icon = MdSessionData.WindowIcon;
+
             Init();
-            this.Height = 640;
-            this.Width = 860;
-            this.Topmost = MdConfig.Main.KeyMenuTopmost;
-            // this.Icon = MdSessionData.WindowIcon;
         }
 
         public void Init()
         {
-            int groupIndex = 0;
-            foreach (var groupName in MdSessionData.CurrentInputMethod.Groups)
-            {
-                var tabPage = new TabPage
-                {
-                    Text = groupName,
-                    Tag = groupIndex,
-                    Content = CreateList(groupIndex++)
-                };
+            DropDown = CreateDropDown();
+            SearchBox = CreateSearch();
+            ListBox = CreateList();
 
-                this.TCMain.Pages.Add(tabPage);
-            }
+            UpdateListBox(GetKeys());
+
+            var layout = new StackLayout
+            {
+                Orientation = Orientation.Vertical
+            };
+
+            layout.Items.Add(new StackLayoutItem(DropDown)
+            {
+                Expand = false,
+                HorizontalAlignment = HorizontalAlignment.Stretch
+            });
+
+            layout.Items.Add(new StackLayoutItem(SearchBox)
+            {
+                Expand = false,
+                HorizontalAlignment = HorizontalAlignment.Stretch
+            });
+
+            layout.Items.Add(new StackLayoutItem(ListBox)
+            {
+                Expand = true,
+                HorizontalAlignment = HorizontalAlignment.Stretch
+            });
+
+            Content = layout;
         }
 
-        private ListControl CreateList(int groupIndex)
+        private IEnumerable<DisplayCharacterContainer.Key> GetKeys()
         {
-            var layout = new ListBox();
-            var keys = MdSessionData
+            return MdSessionData
                 .CurrentInputMethod
                 .Keys
-                .Where((key) => key.GroupIndex == groupIndex);
+                .Where((key) => key.GroupIndex == GroupIndex);
+        }
 
-            foreach (var key in keys)
+        private DropDown CreateDropDown()
+        {
+            var groups = new DropDown();
+
+            foreach (var groupName in MdSessionData.CurrentInputMethod.Groups)
             {
-                layout.Items.Add(key.DisplayText, key.Index.ToString());
+                groups.Items.Add(groupName);
             }
 
-            layout.SelectedKeyChanged += (sender, args) => SelectKey(sender, args);
+            groups.SelectedIndex = GroupIndex;
+            groups.SelectedIndexChanged += OnSelectGroup;
+
+            return groups;
+        }
+
+        private SearchBox CreateSearch()
+        {
+            var search = new SearchBox();
+            search.Load += (sender, args) => search.Focus();
+            search.TextChanged += OnSearch;
+
+            return search;
+        }
+
+        private ListBox CreateList()
+        {
+            var layout = new ListBox();
+            layout.SelectedKeyChanged += OnSelectKey;
 
             return layout;
         }
 
-        public void ReloadInputMethod()
-        {
-            this.Close();
-        }
-
-        private void SelectKey(object sender, EventArgs args)
+        private void OnSelectKey(object sender, EventArgs args)
         {
             MdSessionData.SelectedFromKeyMenu = true;
             var layout = sender as ListBox;
@@ -67,6 +108,39 @@ namespace ArbitesEto2
                 var keyId = Convert.ToInt32(layout.SelectedKey);
                 MdSessionData.KeyMenuKey = KeyGroup.GetKeyFromDisplayId(keyId);
             }
+        }
+
+        private void OnSearch(object sender, EventArgs args)
+        {
+            var search = sender as SearchBox;
+            if (search != null)
+            {
+                var query = search.Text.ToLower();
+                var keys = GetKeys()
+                    .Where((key) => key.DisplayText.ToLower().Contains(query));
+                UpdateListBox(keys);
+            }
+        }
+
+        private void OnSelectGroup(object sender, EventArgs args)
+        {
+            SearchBox.Text = "";
+            GroupIndex = (sender as DropDown).SelectedIndex;
+            UpdateListBox(GetKeys());
+        }
+
+        private void UpdateListBox(IEnumerable<DisplayCharacterContainer.Key> keys)
+        {
+            ListBox.DataStore = null;
+            foreach (var key in keys)
+            {
+                ListBox.Items.Add(key.DisplayText, key.Index.ToString());
+            }
+        }
+
+        public void ReloadInputMethod()
+        {
+            this.Close();
         }
 
         public void ReloadTopmost()
